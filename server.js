9f144aab98bc565050faf7055155af4fcb7c5a12
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-08 13:03:17 +0200

    Refactor depart. endpoint to get data from mongoDB

--- server.js
+++ server.js
@@ -11,13 +11,18 @@ mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUni
     console.log(err);
   }
   else {
-    const app = express();
     console.log('Successfully connected to the database');
-
+    const app = express();
+    const db = client.db('companyDB');
 
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
 
     app.use('/api', employeesRoutes);
     app.use('/api', departmentsRoutes);
