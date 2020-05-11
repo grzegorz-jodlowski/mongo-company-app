fa17aee1a9c6af1789ae2dd15d5dc7729ffa2437
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-11 16:26:04 +0200

    Refactor DB structure (reference to department)

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   } catch (error) {
     res.status(500).json({ message: error });
   }
@@ -14,7 +14,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const employee = await Employee.findOne().skip(rand);
+    const employee = await Employee.findOne().skip(rand).populate('department');
     if (rand) {
       res.json(employee);
     } else {
@@ -27,8 +27,8 @@ router.get('/employees/random', async (req, res) => {
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const employee = await Employee.findById(req.params.id);
-    if (!employee) {
+    const employee = await Employee.findById(req.params.id).populate('department');
+    if (employee) {
       res.json(employee);
     } else {
       res.status(404).json({ message: 'Not Found' });
