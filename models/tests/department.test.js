c9190359bc8660ad12a116a4316b4a77e3f1b9f8
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-13 18:52:12 +0200

    Add more tests for department model

--- models/tests/department.test.js
+++ models/tests/department.test.js
@@ -14,6 +14,41 @@ describe('Department', () => {
     dep.validate(err => {
       expect(err.errors.name).to.exist;
     });
+  });
 
+  it('should throw an error if "name" is not a string', () => {
+    const cases = [{}, []];
+
+    for (let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should throw an error if "name" length is lower than 5 and greater than 20', () => {
+    const cases = ['a', 'aaaa', 'aaaaa aaaaa aaaaa aaaaa'];
+
+    for (let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should not throw an error if "name" has proper format', () => {
+    const cases = ['Management', 'Marketing'];
+
+    for (let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      });
+    }
   });
 });
