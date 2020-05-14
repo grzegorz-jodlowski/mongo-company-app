d6bfd5e55371d02bff2a52bca55121a39aade44e
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-14 14:27:20 +0200

    Add tests for Product model

---
+++ models/tests/product.test.js
@@ -0,0 +1,43 @@
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+const Product = require('../product.model');
+
+after(() => {
+  mongoose.models = {};
+});
+
+describe('Product', () => {
+  const args = ['name', 'client'];
+
+  it('should throw an error if no args', () => {
+    const product = new Product({});
+
+    product.validate(err => {
+      args.forEach(arg => {
+        expect(err.errors[arg]).to.exist;
+      })
+    });
+  });
+
+  it('should throw an error if args are not a strings', () => {
+    const cases = [{}, []];
+
+    for (let element of cases) {
+      const product = new Product({ name: element, client: element });
+      product.validate(err => {
+        args.forEach(arg => {
+          expect(err.errors[arg]).to.exist;
+        })
+      });
+    }
+  });
+
+  it('should not throw an error if args have proper format', () => {
+    const product = new Product({ name: 'Lorem', client: 'Ipsum' });
+
+    product.validate(err => {
+      expect(err).to.not.exist;
+    });
+  });
+});
