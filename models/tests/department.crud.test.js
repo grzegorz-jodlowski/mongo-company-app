ee02742f06d0b59626929a2009180cf3e6fc98d3
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-14 16:13:04 +0200

    Add removing data tests for department collection

--- models/tests/department.crud.test.js
+++ models/tests/department.crud.test.js
@@ -93,4 +93,38 @@ describe('Department crud', () => {
     });
   });
 
+  describe('Removing data', () => {
+    beforeEach(async () => {
+      const testDepOne = new Department({ name: 'Department #1' });
+      await testDepOne.save();
+
+      const testDepTwo = new Department({ name: 'Department #2' });
+      await testDepTwo.save();
+    });
+
+    afterEach(async () => {
+      await Department.deleteMany();
+    });
+
+    it('should properly remove one document with "deleteOne" method', async () => {
+      await Department.deleteOne({ name: 'Department #1' });
+      const deletedDepartment = await Department.findOne({ name: 'Department #1' });
+      expect(deletedDepartment).to.be.null;
+    });
+
+    it('should properly remove one document with "remove" method', async () => {
+      const department = await Department.findOne({ name: 'Department #1' });
+      await department.remove();
+      const removedDepartment = await Department.findOne({ name: 'Department #1' });
+      expect(removedDepartment).to.be.null;
+    });
+
+    it('should properly remove multiple documents with "deleteMany" method', async () => {
+      await Department.deleteMany();
+      const departments = await Department.find();
+      expect(departments.length).to.be.equal(0);
+    });
+
+  });
+
 });
