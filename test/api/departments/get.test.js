31b1cc1d4f630978a9ae9e38381291e246aa3a87
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-15 17:16:18 +0200

    Add tests for get endpoint in departments coll

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,15 +1,16 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../../server.js');
+const server = require('../../../server.js');
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 const request = chai.request;
 
-const Department = require('../../models/department.model');
+const Department = require('../../../models/department.model');
 
 describe('GET /api/departments', () => {
+
   before(async () => {
     const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
     await testDepOne.save();
@@ -22,16 +23,25 @@ describe('GET /api/departments', () => {
     await Department.deleteMany();
   });
 
-  it('/ should return all departments', () => {
-
+  it('/ should return all departments', async () => {
+    const res = await request(server).get('/api/departments');
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body.length).to.be.equal(2);
   });
 
-  it('/:id should return one department by :id ', () => {
-
+  it('/:id should return one department by :id ', async () => {
+    const res = await request(server).get('/api/departments/5d9f1140f10a81216cfd4408');
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.an('object');
+    expect(res.body).to.not.be.null;
   });
 
-  it('/random should return one random department', () => {
-
+  it('/random should return one random department', async () => {
+    const res = await request(server).get('/api/departments/random');
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.an('object');
+    expect(res.body).to.not.be.null;
   });
 
 });
